de5c56b4c0635b4e63b58aa67680d8df2f66bdf9
abdullahbinsaba <abdullahbinsaba2209E@aptechgdn.net>
2024-02-26 20:40:13 +0500

    add all work with home page done

---
+++ App_For_Students/App_For_Students.Models/Models/Messages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_For_Students.Models.Models
+{
+    internal class Messages
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name ="Ocassion Name")]
+        public string Oc_Name { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
