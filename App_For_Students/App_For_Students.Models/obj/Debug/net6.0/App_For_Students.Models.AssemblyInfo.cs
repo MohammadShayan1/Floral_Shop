8cbec8a6e7c806278fd0a833257574f471147213
abdullahbinsaba <abdullahbinsaba2209E@aptechgdn.net>
2024-02-26 13:34:55 +0500

    done work adding project to asp.net

--- App_For_Students/App_For_Students.Models/obj/Debug/net6.0/App_For_Students.Models.AssemblyInfo.cs
+++ App_For_Students/App_For_Students.Models/obj/Debug/net6.0/App_For_Students.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App_For_Students.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acc610562b39f921c429356223be4273cd8b36ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("App_For_Students.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App_For_Students.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
