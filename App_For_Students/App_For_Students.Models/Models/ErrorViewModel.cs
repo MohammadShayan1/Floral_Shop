feb4661a19340f6bc04427c885d958613a8072a9
abdullahbinsaba <abdullahbinsaba2209E@aptechgdn.net>
2024-02-28 12:47:38 +0500

    new  item with admin file and integrated

---
+++ App_For_Students/App_For_Students.Models/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace App_For_Students.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ App_For_Students/App_For_Students/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App_For_Students.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Admin()
+        {
+            return View();
+        }
+    }
+}
