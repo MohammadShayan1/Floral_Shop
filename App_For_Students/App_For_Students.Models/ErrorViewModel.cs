eeeb03a9ebea26c5a5a94e35695cd60588759c76
abdullahbinsaba <abdullahbinsaba2209E@aptechgdn.net>
2024-03-04 13:21:51 +0500

    removed old

--- App_For_Students/App_For_Students.Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace App_For_Students.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
